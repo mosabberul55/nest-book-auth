1ffb3b44b69dcdd8d80338d8752832a66881e0a5
mosabberul55 <mosabberulislam55@gmail.com>
2025-01-07 16:10:44 +0600

    auth init ~

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AuthorModule } from './author/author.module';
 import { TagModule } from './tag/tag.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { TagModule } from './tag/tag.module';
     BookModule,
     AuthorModule,
     TagModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
