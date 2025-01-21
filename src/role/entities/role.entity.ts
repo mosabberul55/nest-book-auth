3e2c277854964d1efcf3f1d9df86992394a48d19
mosabberul55 <mosabberulislam55@gmail.com>
2025-01-21 13:52:22 +0600

    auth done ~

--- src/role/entities/role.entity.ts
+++ src/role/entities/role.entity.ts
@@ -2,7 +2,7 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Action } from '../enums/action.enum';
 import { Resource } from '../enums/resource.enum';
 
-@Schema({ timestamps: true })
+@Schema()
 class Permission {
   @Prop({ required: true, enum: Resource })
   resource: Resource;
